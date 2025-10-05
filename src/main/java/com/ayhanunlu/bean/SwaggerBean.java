c30327d15c68ad8373ca7cfadb3b833257ef1595
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-10-05 15:41:11 +0300

    SwaggerConfig.java SwaggerBean.java added and functional

---
+++ src/main/java/com/ayhanunlu/bean/SwaggerBean.java
@@ -0,0 +1,19 @@
+package com.ayhanunlu.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@Component
+public class SwaggerBean {
+
+    private String title = "ATM Management API";
+    private String description = "API documentation for ATM project";
+    private String version = "v1.0";
+}
---
+++ src/main/java/com/ayhanunlu/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.ayhanunlu.config;
+
+import com.ayhanunlu.bean.SwaggerBean;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Autowired
+    private SwaggerBean swaggerBean;
+
+    @Bean
+    public OpenAPI atmOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title(swaggerBean.getTitle())
+                        .description(swaggerBean.getDescription())
+                        .version(swaggerBean.getVersion()));
+    }
+}
